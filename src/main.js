cab2a6177533dcea9c78a798bc8d7b77481c4988
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-04-21 21:41:36 +0300

    adaptive for phone, tablet added

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 const mainMenu = document.querySelector('.nav__mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
+const navLinks = document.querySelectorAll('.nav__link');
+
+function closeLinks() {
+  navLinks.forEach((element) => {
+    element.addEventListener('click', () => {
+      mainMenu.style.top = '-100%';
+    });
+  });
+}
 
 function show() {
   mainMenu.style.display = 'flex';
   mainMenu.style.top = '0';
+  closeLinks();
 }
 
 function close() {
